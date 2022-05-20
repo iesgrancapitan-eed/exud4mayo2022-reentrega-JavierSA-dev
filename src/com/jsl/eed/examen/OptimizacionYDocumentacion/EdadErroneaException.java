6635f09a28aea0646f8847b721999d59c08e3c10
javierSA <javilondo56@gmail.com>
2022-05-20 17:41:48 +0200

    Comentarios JavaDOC de la excepcion

--- src/com/jsl/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
+++ src/com/jsl/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -1,8 +1,11 @@
 package com.jsl.eed.examen.OptimizacionYDocumentacion;
-
+/**
+ * Se lanza si la edad es menor a cero 
+ * @param message Mensaje de error
+ */
 public class EdadErroneaException extends Exception {
     /**
-     * Se lanza si la edad es menor a cero con un mensaje de error
+     * Crea la exepción con un mensaje de error 
      * @param message Mensaje de error
      */
 	public EdadErroneaException(String message) {
