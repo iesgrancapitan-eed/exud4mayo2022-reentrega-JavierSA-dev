24d6e1560d77485f4ec02beb8aad6626cec3b831
javierSA <javilondo56@gmail.com>
2022-05-20 17:22:13 +0200

    docs: documentación javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jsl.eed.examen.OptimizacionYDocumentacion"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.jsl.eed.examen.OptimizacionYDocumentacion","l":"EdadErroneaException"},{"p":"com.jsl.eed.examen.OptimizacionYDocumentacion","l":"Mascota"},{"p":"com.jsl.eed.examen.OptimizacionYDocumentacion","l":"Perro"}];updateSearchResults();
